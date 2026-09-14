fc28fec0aa5f35dafbbf4bac78b583df431cb1e3
wiseza <wiphawi_ri@kkumail.com>
2026-09-15 00:59:40 +0700

    Update DepositService.java

--- lab9/src/main/java/com/example/lab9/service/DepositService.java
+++ lab9/src/main/java/com/example/lab9/service/DepositService.java
@@ -1,5 +1,41 @@
 package com.example.lab9.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.lab9.model.Account;
+import com.example.lab9.model.DepositTransaction;
+import com.example.lab9.repository.AccountRepository;
+import com.example.lab9.repository.DepositRepository;
+
+@Service
 public class DepositService {
-    
+
+    private final AccountRepository accountRepository;
+    private final DepositRepository depositRepository;
+
+    public DepositService(
+            AccountRepository accountRepository,
+            DepositRepository depositRepository) {
+
+        this.accountRepository = accountRepository;
+        this.depositRepository = depositRepository;
+    }
+
+    //@Transactional
+    public void deposit(Long accountId, Double amount) {
+
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+
+        account.setBalance(account.getBalance() + amount);
+        accountRepository.save(account);
+
+        DepositTransaction transaction = new DepositTransaction();
+        transaction.setAmount(amount);
+        transaction.setAccount(account);
+
+        depositRepository.save(transaction);
+
+        throw new RuntimeException("Test Rollback");
+    }
 }
