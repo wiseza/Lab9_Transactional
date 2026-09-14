e1eaf71e1927c3e42423923fa6f43e89a0958e6b
wiseza <wiphawi_ri@kkumail.com>
2026-09-15 00:59:44 +0700

    Update DepositRepository.java

--- lab9/src/main/java/com/example/lab9/repository/DepositRepository.java
+++ lab9/src/main/java/com/example/lab9/repository/DepositRepository.java
@@ -1,5 +1,9 @@
 package com.example.lab9.repository;
 
-public class DepositRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.lab9.model.DepositTransaction;
+
+public interface DepositRepository extends JpaRepository<DepositTransaction, Long> {
+
 }
